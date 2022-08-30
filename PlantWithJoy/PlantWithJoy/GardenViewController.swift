4b4512768b81604afa9e822494dc5795260fcc2a
Jay <103009135+Rookie0031@users.noreply.github.com>
2022-08-30 20:55:43 +0900

    [Design] - hi fi 1차 마무리/개발시작

---
+++ PlantWithJoy/PlantWithJoy/GardenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantWithJoy
+//
+//  Created by 장지수 on 2022/08/09.
+//
+
+import UIKit
+
+class GardenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
